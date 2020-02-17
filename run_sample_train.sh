3849941a64e3a21d5403418d168232a179c53f08
Nikolai Yakovenko <nyakovenko@nvidia.com>
2020-02-17 15:50:08 -0500

    mini -- more options for training from pre-trained 8x8 model

--- run_sample_train.sh
+++ run_sample_train.sh
@@ -12,14 +12,20 @@ num_classes=8
 bn_batches=200 # default
 bn_batches=20 # smaller data...
 # Checkpoints -- where to save and how often?
-check_period=10
+check_period=100
+# Default 8x data workers -- more if slow, and 1x for debugging.
+data_workers=8
+data_workers=16
+#data_workers=1
+batch_size=5
 
 cmd="python tools/run_net.py \
   --cfg configs/Kinetics/SLOWFAST_8x8_R50.yaml \
   NUM_GPUS 1 TRAIN.ENABLE True TEST.ENABLE False \
+  DATA_LOADER.NUM_WORKERS $data_workers \
   DATA.PATH_TO_DATA_DIR data/edge-100/  \
   TRAIN.CHECKPOINT_FILE_PATH $model_dir/$model_path \
-  TRAIN.CHECKPOINT_TYPE caffe2 TRAIN.BATCH_SIZE 4 \
+  TRAIN.CHECKPOINT_TYPE caffe2 TRAIN.BATCH_SIZE $batch_size \
   TRAIN.CHECKPOINT_PERIOD $check_period \
   MODEL.NUM_CLASSES $num_classes \
   BN.NUM_BATCHES_PRECISE $bn_batches"
